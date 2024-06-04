2098824599d5e2f6936a5a0bda1e6786cfff2321
imyousuf18 <91778378+imyousuf18@users.noreply.github.com>
2024-06-05 02:09:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const notifyButton = document.getElementById("notify-btn");
+
+notifyButton.addEventListener("click", function() {
+  alert("Apology agar accept karli hai to Kindly Whatsapp pe Raabta Qaim krein wahan aapka Intezaar Jaari hai :))).");
+});
+
+const notifyButton1 = document.getElementById("notify-btnn");
+
+notifyButton1.addEventListener("click", function() {
+  alert("Pleeeeeeaaaaaaasssssssseeeeeeeee???????????");
+});
